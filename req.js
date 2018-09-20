ad7d295b7f3fe3dfcdca02b2a2d136591097b248
Shawn <linxiang08@gmail.com>
2018-09-20 19:43:02 -0400

    adas

--- req.js
+++ req.js
@@ -1,9 +1,31 @@
 let display = document.querySelector('.display');
 let uploadInput = document.getElementById('input');
 let displayString = '';
-uploadInput.addEventListener('change', e => {
+
+// counter 
+let schema = 0;
+let host = 0;
+let port = 0;
+let path = 0;
+let query = 0;
+let fragment = 0;
+let userName = 0;
+
+
+// 
+
+// 
+let proto = "protocol";
+let hostname = "hostname";
+
+// 
+
+uploadInput.addEventListener('change', e =>  {
   console.log(uploadInput.files[0])
     readeFiles(uploadInput.files[0]);
+    console.log(uploadInput.files[0]);
+
+
 
 }, false);
 
@@ -11,9 +33,8 @@ function readeFiles(files){
 const reader = new FileReader();
 reader.onload = ()=> {
   let resultSet = reader.result.split('\n');
- console.log(resultSet);
  
-   parseURl(resultSet);
+    parseURL(resultSet);
 
   
   
@@ -52,16 +73,79 @@ async function getURL(url) {
 }
 
 function parseURL(set) {
-    set.forEach(ele => {
-        let url = new URL(ele);
+        let result = [];
+     for(let i = 0; i < set.length; i++)
+    result[i].push(builder(new URL(element)));
+          
+    
+            
+        result;
         
-        
-    });
 }
 
+function builder(ele) {
+    let poto = ele.protocol;
+    if (poto !="") {
+        schema++;
+    }
+    let username = ele.username;
+    if (username != "") {
+        userName ++;
+        
+    }
+    let HOst = ele.host;
+    if (HOst != "") {
+        host++;
+        
+    }
+    let Port = ele.port;
+     if (Port !="") {
+         port++;
+         
+     }
+    let pn = ele.pathname;
+    if (pn !="") {
+       path++;
+    }
+    let sch = ele.search;
+    if (sch !="") {
+        query++;
+    }
+    let has =   ele.hash;
+    if (has != "") {
+        fragment++;
+    }
+
+    let table = `<table>
+ <tr><th>url: ${ele.href}</th></tr>
+<tr><th>schema</th> <td>${poto}</td>  </tr>
+<tr><th>userinfo</th> <td>${username}</td>  </tr>
+<tr><th>host</th> <td>${HOst}</td>  </tr>
+<tr><th>port</th> <td>${Port}</td>  </tr>
+<tr><th>path</th> <td>${pn}</td>  </tr>
+<tr><th>query</th> <td>${sch}</td>  </tr>
+<tr><th>fragment</th> <td>${has}</td>  </tr>
+</table>
+`;
+
+console.log(table);
+
+return table;
 
 
-getURL('www.google.com').then(parseURL);
+  
+
+
+
+}
+
+let final = parseURL();
+console.log(final);
+let data =  final.reduce((acc,next)=>{
+    return acc+ next;
+}
+);
+display.innerHTML = data;
 
 
 
